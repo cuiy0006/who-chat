12453fc0c356ba1ecf54f799a02f45449e8ed726
cuiy0006 <yc2554@nyu.edu>
2019-06-30 15:11:33 -0400

    revised

---
+++ client_makefile.mk
@@ -0,0 +1,16 @@
+CC = g++
+CPPFLAGS = -std=c++11
+INCLUDE=-Isrc/common
+
+who_chat_client: src/client/client-main.o src/client/client.o
+	$(CC) $(CPPFLAGS) -c $^ -o $@
+
+src/client/client-main.o: src/client/client-main.cpp
+	$(CC) $(CPPFLAGS) $(INCLUDE) -c $^ -o $@
+
+src/client/client.o: src/client/client.cpp
+	$(CC) $(CPPFLAGS) $(INCLUDE) -c $^ -o $@
+
+clean:
+	rm -rf *.o who_chat_client
+	rm -rf src/client/*.o
---
+++ server_makefile.mk
@@ -0,0 +1,16 @@
+CC = g++
+CPPFLAGS = -std=c++11
+INCLUDE=-Isrc/common
+
+who_chat_server: src/server/server-main.o src/server/server.o
+	$(CC) $(CPPFLAGS) -c $^ -o $@
+
+src/server/server-main.o: src/server/server-main.cpp
+	$(CC) $(CPPFLAGS) $(INCLUDE) -c $^ -o $@
+
+src/server/server.o: src/server/server.cpp
+	$(CC) $(CPPFLAGS) $(INCLUDE) -c $^ -o $@
+
+clean:
+	rm -rf *.o who_chat_server
+	rm -rf src/server/*.o
